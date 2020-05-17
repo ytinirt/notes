820876aa506afb2101942ce5bfb2b944dfb79487
ZHAO Yao <ytinirt@qq.com>
2020-05-17 23:10:53 +0800

    update

--- .travis/update-toc.sh
+++ .travis/update-toc.sh
@@ -2,9 +2,15 @@
 
 set -x
 echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}"
-echo "TRAVIS_COMMIT_MESSAGEsss=${TRAVIS_COMMIT_MESSAGE}"
+echo "TRAVIS_COMMIT_MESSAGE=${TRAVIS_COMMIT_MESSAGE}"
+
 if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
     echo "give up updating TOC @ branch ${TRAVIS_BRANCH}"
+    exit 0
+fi
+
+if [[ "${TRAVIS_COMMIT_MESSAGE:0:16}" = "Travis CI build:" ]]; then
+    echo "bypass travis commit: ${TRAVIS_COMMIT_MESSAGE}"
     exit 0
 fi
 
@@ -17,4 +23,4 @@ git add ./README.md
 git commit --message "Travis CI build: $TRAVIS_BUILD_NUMBER"
 
 git remote add origin-ci https://${GH_TRAVIS_CI_TOKEN}@github.com/ytinirt/notes.git
-git push --set-upstream origin-ci master
+git push --set-upstream origin-ci HEAD:master
